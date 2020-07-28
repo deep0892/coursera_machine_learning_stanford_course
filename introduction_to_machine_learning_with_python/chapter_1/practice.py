06cf1218d2f311349836b96107c499d0368875a4
Deepankar <deepankar080892@gmail.com>
2020-07-28 11:49:06 +0530

    Intro to numpy scipy

---
+++ introduction_to_machine_learning_with_python/chapter_1/practice.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import sparse
+
+# numpy array
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print("x:\n{}".format(x))
+
+# Create a 2D NumPy array with a diagonal of ones and zero everywhere else (identity matrix)
+eye = np.eye(4)
+print("NumPy array: \n{}".format(eye))
+
+# Convert the NumPy array to a SciPy sparse matrix in CSR format
+# Only the nonzero entries are stored
+sparse_matrix = sparse.csr_matrix(eye)
+print("\nSciPy sparse CSR matrix:\n{}".format(sparse_matrix))
+
+# Create sparse matrix using COO format:
+data = np.ones(4)
+row_indices = np.arange(4)
+col_indices = np.arange(4)
+eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
+print("COO representation\n{}".format(eye_coo))
