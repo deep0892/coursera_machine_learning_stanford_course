4f3d6600bf80b968b89cd07e6f868f861d326700
Deepankar <deepankar080892@gmail.com>
2020-07-28 16:31:37 +0530

    matplotlib

--- introduction_to_machine_learning_with_python/chapter_1/practice.py
+++ introduction_to_machine_learning_with_python/chapter_1/practice.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import sparse
+import matplotlib.pyplot as plt
 
 # numpy array
 x = np.array([[1, 2, 3], [4, 5, 6]])
@@ -20,3 +21,10 @@ row_indices = np.arange(4)
 col_indices = np.arange(4)
 eye_coo = sparse.coo_matrix((data, (row_indices, col_indices)))
 print("COO representation\n{}".format(eye_coo))
+
+# Generate a sequence of number from -10 to 10 with 100 step in between
+x = np.linspace(-10, 10, 100)
+# Create a second array using sine
+y = np.sin(x)
+# The plot function makes a line chart of one array against another
+plt.plot(x,y, marker="x")
